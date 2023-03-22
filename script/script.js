6b059054335852624c98117c7f42b163c3b5d52f
Teddy Nakad <tnakad@my.bcit.ca>
2023-03-22 12:35:44 -0700

    default parameter

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const greet = (name , time = '2:00') => {
+    console.log(`Hello ${time} ${name}`)
+}
+
+// function greet(parameter) {
+//     console.log("Hello " + parameter)
+// }
+
+greet('Teddy')
+
+
