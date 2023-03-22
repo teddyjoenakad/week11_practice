36f1ebeec36793babec57d62da9c9342e4a05395
Teddy Nakad <tnakad@my.bcit.ca>
2023-03-22 14:22:15 -0700

    inserting new li to ul

--- script/script.js
+++ script/script.js
@@ -9,16 +9,47 @@
 // greet('Teddy')
 
 
-const area = (radius) => {
-    if (isNaN(radius)) {
-        alert('This is not a number')
-    }
-    else{
-    const area = ((radius**2)*Math.PI).toFixed(2)
-    return area
-    }
-};
 
-let radius = prompt('Enter the radius of the cirle you want to calculate the area:');
 
-alert(`The area of a cirlce with radius ${radius} is ${area(radius)}`);
+// const radiusPara = document.querySelector('#radius');
+// console.log(radiusPara);
+// const areaPara = document.querySelector('#area');
+// console.log(areaPara);
+
+// const area = (radius) => {
+//     if (isNaN(radius)) {
+//         areaPara.textContent = 'This is not a number';
+//         return;
+//     }
+//     else{
+//     const area = ((radius**2)*Math.PI).toFixed(2);
+//     return area;
+//     }
+// };
+
+// let radius = prompt('Enter the radius of the cirle you want to calculate the area:');
+
+// radiusPara.textContent += radius;
+
+// if (area(radius) !== undefined) {
+    
+// }
+// else{
+//     areaPara.textContent = `The area of a cirlce with radius ${radius} is ${area(radius)}`;
+// }
+
+
+
+const populatelist = (shoppinglist) => {
+    const ulList = document.querySelector('.shopping');
+
+    for (let item of shoppinglist){
+        const newLi = document.createElement('li');
+        ulList.appendChild(newLi);
+        newLi.textContent = item;
+    }
+}
+
+let mylist = ['bread', 'cheese', 'green pepper']
+
+populatelist(mylist)
