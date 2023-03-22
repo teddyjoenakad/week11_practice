41fb489712c37c08cc5e6a2971052eb13b82cc3b
Teddy Nakad <tnakad@my.bcit.ca>
2023-03-22 13:25:56 -0700

    function to calculate area of circle with user radius prompt

--- script/script.js
+++ script/script.js
@@ -1,11 +1,24 @@
-const greet = (name , time = '2:00') => {
-    console.log(`Hello ${time} ${name}`)
-}
+// const greet = (name , time = '2:00') => {
+//     console.log(`Hello ${time} ${name}`)
+// }
 
 // function greet(parameter) {
 //     console.log("Hello " + parameter)
 // }
 
-greet('Teddy')
+// greet('Teddy')
 
 
+const area = (radius) => {
+    if (isNaN(radius)) {
+        alert('This is not a number')
+    }
+    else{
+    const area = ((radius**2)*Math.PI).toFixed(2)
+    return area
+    }
+};
+
+let radius = prompt('Enter the radius of the cirle you want to calculate the area:');
+
+alert(`The area of a cirlce with radius ${radius} is ${area(radius)}`);
